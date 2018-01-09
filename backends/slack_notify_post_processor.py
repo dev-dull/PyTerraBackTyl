451fe137090339acd2c464d632caec41f62ef1b4
Alastair Drong <alastair.drong@infogroup.com>
2018-01-08 17:30:18 -0800

    Consolidate redundant code, add :fancy_slack_emoji_stuff:

--- backends/slack_notify_post_processor.py
+++ backends/slack_notify_post_processor.py
@@ -34,7 +34,7 @@ class SlackNotifyPostProcessor(TYLNonpersistent):
         """
         self.lock_info = state_obj
         if self.lock_info['Operation'] in self.C.SLACK_NOTIFICATIONS_ON_TF_ACTIONS:
-            self._post_to_slack(self.C.SLACK_NOTIFY_FORMAT.format(**defaultdict(str, self.lock_info)), '*LOCKED*')
+            self._post_to_slack(self.C.SLACK_NOTIFY_FORMAT.format(**defaultdict(str, self.lock_info)), '*LOCKED* :lock:')
 
     def on_unlocked(self, state_obj, **kwargs):
         """
@@ -42,7 +42,7 @@ class SlackNotifyPostProcessor(TYLNonpersistent):
         :param kwargs: Includes 'raw' json object as text (str)
         """
         if self.lock_info['Operation'] in self.C.SLACK_NOTIFICATIONS_ON_TF_ACTIONS and self.C.SLACK_NOTIFY_UNLOCK:
-            self._post_to_slack(self.C.SLACK_NOTIFY_UNLOCK_FORMAT.format(**defaultdict(str, self.lock_info)), '*Unlock*')
+            self._post_to_slack(self.C.SLACK_NOTIFY_UNLOCK_FORMAT.format(**defaultdict(str, self.lock_info)), '*Unlock* :unlock:')
         self.lock_info = {}
 
     def process_tfstate(self, tfstate_obj, **kwargs):
@@ -54,7 +54,7 @@ class SlackNotifyPostProcessor(TYLNonpersistent):
         if self.C.SLACK_NOTIFY_HOST_LIST:
             new_hosts = ', '.join(TYLHelpers.get_hostnames_from_tfstate(tfstate_obj))
             if new_hosts:
-                self._post_to_slack(new_hosts, 'Created Hosts')
+                self._post_to_slack(new_hosts, 'Created Hosts :lock_with_ink_pen: ')
 
     def post_processor_status(self):
         """
