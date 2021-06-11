52c1af64a87e63ddc0faf25befbd86d3af643d72
Alastair Drong <adrong@gmail.com>
2021-06-11 09:27:45 -0700

    Set Git user information on startup to prevent service errors.

--- ds.sh
+++ ds.sh
@@ -13,6 +13,25 @@ if [ ! -f ~/.ssh/id_rsa.pub ]; then
     ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa <<< y > /dev/null 2>&1
 fi
 
+# Set the user information so Git won't complain
+# (when not set, git exits with non-zero, raising service exception)
+user_name=`git config --global user.name`
+if [ -z "$user_name" ]; then
+    if [ -z "$GIT_USER_NAME" ]; then
+        GIT_USER_NAME="pyterrabacktyl"
+    fi
+    git config --global user.name "$GIT_USER_NAME"
+fi
+
+user_email=`git config --global user.email`
+if [ -z "$user_email" ]; then
+    if [ -z "$GIT_USER_EMAIL" ]; then
+        GIT_USER_EMAIL="pyterrabacktyl@devdull.lol"  # Default to script author's domain.
+    fi
+    git config --global user.name "$GIT_USER_EMAIL"
+fi
+
+
 echo GIT BACKEND USERS: Please make note of your public SSH key >&2
 cat ~/.ssh/id_rsa.pub >&2
 
